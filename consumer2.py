77a2c8649b8e31c209114e135f243dffe3b372c6
sabkapapa <42075338+sabkapapa@users.noreply.github.com>
2019-11-29 15:11:56 +0530

    Update consumer2.py

--- consumer2.py
+++ consumer2.py
@@ -17,20 +17,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-received_data = "("
-i = 0
-for message in consumer:
-message = message.value
-print(message)
-if "list" not in message:
-continue
-st_temp = loads(message)
-print(st_temp["list"])
-received_data += "Temperature: at Bangalore  " + str(st_temp["list"][0]["main"]["temp"])
-i += 1
-break
-received_data += ')'
-return received_data
+    received_data = "("
+    i = 0
+    for message in consumer:
+        message = message.value
+        print(message)
+        if "list" not in message:
+            continue
+        st_temp = loads(message)
+        print(st_temp["list"])
+        received_data += "Temperature: at Bangalore  " + str(st_temp["list"][0]["main"]["temp"])
+        i += 1
+        break
+    received_data += ')'
+    return received_data
 
 if __name__ == '__main__':
 app.run()
